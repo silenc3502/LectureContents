544b474f091e2840503dac292ca05dbaa6426269
jaqkqh15 <jaqkqh15@naver.com>
2021-07-07 21:52:04 +0900

    .

---
+++ javascript/JungahHwang/frontend/src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div>
+      <router-link :to="{ name: 'Home' }" class="nav-link" active-class="active">Home</router-link>
+      <router-link :to="{ name: 'Test' }" class="nav-link" active-class="active">Test</router-link>
+    </div>
+    <h2>테스트 페이지</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  component: {
+
+  },
+  data () {
+    return {
+      msg: 'Test'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>

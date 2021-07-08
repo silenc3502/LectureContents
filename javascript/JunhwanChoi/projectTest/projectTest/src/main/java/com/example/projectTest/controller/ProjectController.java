159caeb2b91253a6d751ed6774f40065e1580361
ChoiJunHwanzz <wnsghks884@gmail.com>
2021-07-08 19:01:57 +0900

    [최준환]43일차 복습

--- javascript/JunhwanChoi/projectTest/projectTest/src/main/java/com/example/projectTest/controller/ProjectController.java
+++ javascript/JunhwanChoi/projectTest/projectTest/src/main/java/com/example/projectTest/controller/ProjectController.java
@@ -15,19 +15,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 
-
-import com.example.projectTest.entity.Project;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 
 @Slf4j

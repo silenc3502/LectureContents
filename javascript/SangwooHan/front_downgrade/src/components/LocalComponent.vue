35346a5cb370f0c409c5a21f387004f7d450991d
bombom23 <tkddn5678@naver.com>
2021-07-09 15:14:49 +0900

    [한상우] 7월 8일 vue 복습

---
+++ javascript/SangwooHan/front_downgrade/src/components/LocalComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>{{ num }}</div>
+</template>
+
+<script>
+export default {
+    name: 'LocalComponent',
+    props: {
+        num: Number,
+       monster: String
+    }
+}
+
+
+</script>

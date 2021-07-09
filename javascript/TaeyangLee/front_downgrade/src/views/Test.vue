025247e27602113aff59a75a096e6e0d169a9915
rudghxodid <rudghxodid@naver.com>
2021-07-09 15:33:44 +0900

    change

--- javascript/TaeyangLee/front_downgrade/src/views/Test.vue
+++ javascript/TaeyangLee/front_downgrade/src/views/Test.vue
@@ -1,11 +1,6 @@
-<!-- HTML 요소 파트 작성 - Vue랑 연결 시킬 형식으로 작성해도 무방 -->
 <template>
     <div class="home">
         <div id="header">
-            <!-- router-link는 어디로 갈지 URL 맵핑해주는 Spring의 Controller 같은 역할
-                 to: Home은 이 녀석을 클릭했을 때 router/index.js에 등록된 Home으로 가게 만듬 
-                 class: nav-link의 경우 Navigation Bar 같은 역할을 해줄 수 있게 구성함을 의미
-                 active-class: 링크가 활성화 되었을때 적용된 CSS를 의미함 -->
             <router-link :to="{ name: 'Home' }"
                     class="nav-link"
                     active-class="active">
@@ -30,9 +25,11 @@
     </div>
 </template>
 
-<!-- javascript 관련 파트 작성 -->
 <script>
+import Vue from 'vue'
+import GlobalComponent from '../components/GlobalComponent.vue'
 import LocalComponent from '../components/LocalComponent.vue'
+Vue.component(GlobalComponent.name, GlobalComponent)
 export default {
     components: {
         'local-component': LocalComponent

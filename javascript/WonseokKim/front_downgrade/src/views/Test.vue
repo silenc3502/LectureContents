a4bcc84d79231c76ff21161210d5b2000b25d883
wsk0307 <wsk0307@naver.com>
2021-07-09 14:52:45 +0900

    44일차 수업 복습

--- javascript/WonseokKim/front_downgrade/src/views/Test.vue
+++ javascript/WonseokKim/front_downgrade/src/views/Test.vue
@@ -1,11 +1,6 @@
-<!-- HTML 요소 파트 작성 - Vue랑 연결 시킬 형식으로 작성해도 무방 -->
 <template>
     <div class="home">
         <div id="header">
-            <!-- router-link는 어디로 갈지 URL 맵핑해주는 Spring의 Controller 같은 역할
-                 to: Home은 이 녀석을 클릭했을 때 router/index.js에 등록된 Home으로 가게 만듬 
-                 class: nav-link의 경우 Navigation Bar 같은 역할을 해줄 수 있게 구성함을 의미
-                 active-class: 링크가 활성화 되었을때 적용된 CSS를 의미함 -->
             <router-link :to="{ name: 'Home' }"
                     class="nav-link"
                     active-class="active">
@@ -25,22 +20,32 @@
             </global-component>
 
             <local-component v-bind:num="value"></local-component>
-            <button v-on:click="plus">클릭해봐!</button>
+            <button v-on:click="plus">클릭해봐!</button><br><br>
+            
+            <global-component v-bind:add-monster="monsterCounter">
+            </global-component>
+
+            
         </div>
     </div>
 </template>
 
-<!-- javascript 관련 파트 작성 -->
 <script>
+import Vue from 'vue'
+import GlobalComponent from '../components/GlobalComponent.vue'
 import LocalComponent from '../components/LocalComponent.vue'
+import GlobalComponent from '../components/GlobalComponent.vue'
+Vue.component(GlobalComponent.name, GlobalComponent)
 export default {
     components: {
-        'local-component': LocalComponent
+        'local-component': LocalComponent,
+        GlobalComponent
     },
     data () {
         return {
             counter: 0,
             value: 0,
+            monsterCounter: 0,
             msg: 'Vue 객체 데이터'
         }
     },

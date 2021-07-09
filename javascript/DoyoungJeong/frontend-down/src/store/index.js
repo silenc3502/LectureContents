0245452eed143afb28fc45c2fab862761fbb4646
jdy8739 <jdy8739@naver.com>
2021-07-09 21:39:59 +0900

    뷰 추가

---
+++ javascript/DoyoungJeong/frontend-down/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+}) 옆에처럼 일반 다른 파일밖에 안잡힙니다

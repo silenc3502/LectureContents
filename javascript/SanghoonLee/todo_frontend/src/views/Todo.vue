6ed89b82a12b88c465b13f6c4ac98d04536c8a92
silenc3502 <gcccompil3r@gmail.com>
2021-07-12 17:20:23 +0900

    todo header

---
+++ javascript/SanghoonLee/todo_frontend/src/views/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="todo">
+        <todo-header></todo-header>
+    </div>
+</template>
+
+<script>
+
+import TodoHeader from '../components/todo/TodoHeader.vue'
+
+export default {
+    name: 'Todo',
+    components: {
+        'todo-header': TodoHeader
+    }
+}
+
+</script>
+
+<style>
+
+body {
+    text-align: center;
+    background-color: #CEDEBD;
+}
+
+</style>

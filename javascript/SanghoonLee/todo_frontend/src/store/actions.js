7752db62ebda9dfbd4a1b918ea65b0b5072595e7
silenc3502 <gcccompil3r@gmail.com>
2021-07-12 19:05:48 +0900

    Todo 입력처리

---
+++ javascript/SanghoonLee/todo_frontend/src/store/actions.js
@@ -0,0 +1,16 @@
+import {
+    ADD_TODO
+} from './mutation-types'
+
+export default {
+    addTodo (context, payload) {
+        context.commit(ADD_TODO, payload)
+    },
+    save ({ state }) {
+        const data = {
+            todoItems: state.todoItems,
+            nextTodoId: state.nextTodoId
+        }
+        localStorage.setItem('todo-app-data', JSON.stringify(data))
+    }
+}
--- javascript/SanghoonLee/todo_frontend/src/views/Todo.vue
+++ javascript/SanghoonLee/todo_frontend/src/views/Todo.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="todo">
         <todo-header></todo-header>
+        <!-- todo-input 컴포넌트가 emit(addTodo, ~~)를 하면 onAddTodo()가 동작 -->
         <todo-input v-on:addTodo="onAddTodo"></todo-input>
     </div>
 </template>
@@ -8,11 +9,26 @@
 <script>
 
 import TodoHeader from '../components/todo/TodoHeader.vue'
+import TodoInput from '../components/todo/TodoInput.vue'
+
+import { mapActions } from 'vuex'
 
 export default {
     name: 'Todo',
     components: {
-        'todo-header': TodoHeader
+        'todo-header': TodoHeader,
+        'todo-input': TodoInput
+    },
+    methods: {
+        ...mapActions ([
+            'addTodo',
+            'save'
+        ]),
+        onAddTodo (content) {
+            const todoItem = { content }
+            this.addTodo(todoItem)
+            this.save()
+        }
     }
 }
 

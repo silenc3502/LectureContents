1ac3915bd18fde4cfc647293f7ae6d84e7ad222a
siary11 <siary11@naver.com>
2021-07-13 21:21:12 +0900

    [김남교] TODOLIST  + 몬스터 1마리 추가/삭제 기능 완료 (수업내용)

---
+++ javascript/NamkyoKim/frontenddown/src/components/homework/HomeworkHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <h2>Homework 헤더</h2>
+    <p>{{ msg }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return{
+            msg: '판타지 세계에 오신것을 환영합니다'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h2{
+    color: #42b983;
+}
+
+div{
+    background: color #77cce1;
+}
+</style>
---
+++ javascript/NamkyoKim/frontenddown/src/store/states.js
@@ -0,0 +1,10 @@
+export default {
+    // TODO
+    todoItems: [],
+    editingId: 0,
+    nextTodoId: 1,
+    filter: null,
+    // 몬스터
+    monsterElements: [],
+    nextMonsterId: 1
+}

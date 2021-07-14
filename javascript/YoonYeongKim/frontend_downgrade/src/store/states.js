184173fbd49c914a2b23d2ee6402d74abc961328
YoonYeongKim1 <03y20y@naver.com>
2021-07-14 15:28:57 +0900

    47일차 : TodoList 완성, Monster 게임 예제

--- javascript/YoonYeongKim/frontend_downgrade/src/store/states.js
+++ javascript/YoonYeongKim/frontend_downgrade/src/store/states.js
@@ -1,4 +1,11 @@
 // 실제 데이터 관리하는 장소
 export default {
-    todoItems: []
+    // TODO
+    todoItems: [],
+    editingId: 0,
+    nextTodoId: 1,
+    filter: null,
+    // 몬스터
+    monsterElements: [],
+    nextMonsterId: 1
 } 

83beef5c3f51e9340daa5622e65267cb6f94e8aa
KH-CSH <endras132@naver.com>
2021-07-14 22:50:35 +0900

    48일차 내용

--- javascript/Seunghyeonchoi/front_downgrade/src/views/Test.vue
+++ javascript/Seunghyeonchoi/front_downgrade/src/views/Test.vue
@@ -26,16 +26,24 @@
 
 
             <local-component v-bind:num="value"></local-component>
-            <button v-on:click="plus">클릭해봐!</button>
+            <button v-on:click="plus">클릭해봐!</button><br>
 
+            <b>random: {{ this.$store.getters.randomFromSpring }}</b><br>
+            <input type="button" @click="randomNumber()" value="random"/><br>
         </div>
     </div>
 </template>
 
 <!-- javascript 관련 파트 작성 -->
 <script>
+import Vue from 'vue'
+import GlobalComponent from '../components/GlobalComponent.vue'
 import LocalComponent from '../components/LocalComponent.vue'
 
+import { mapActions } from 'vuex'
+
+// import { mapActions } from 'vuex'
+Vue.component(GlobalComponent.name, GlobalComponent)
 export default {
     components: {
         'local-component': LocalComponent
@@ -48,8 +56,14 @@ export default {
         }
     },
     methods: {
+        ...mapActions ([
+            'generateRandomNumber'
+        ]),
         plus () {
             this.value++
+        },
+        randomNumber () {
+            this.generateRandomNumber()
         }
     }
 }

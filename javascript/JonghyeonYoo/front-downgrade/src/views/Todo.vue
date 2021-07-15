8be088f3125d36aa380e47fec065ec24a642678d
yoo-jong-hyeon <gowert612@gmail.com>
2021-07-15 15:22:09 +0900

    [유종현] 48일차 수업 복습

--- javascript/JonghyeonYoo/front-downgrade/src/views/Todo.vue
+++ javascript/JonghyeonYoo/front-downgrade/src/views/Todo.vue
@@ -5,8 +5,10 @@
         <todo-input v-on:addTodo="onAddTodo"></todo-input>
         <todo-list
                 v-on:removeTodo="onRemoveTodo"
-                v-on:editTodo="onEditTodo">
+                v-on:editTodo="onEditTodo"
+                v-on:toggleTodoStatus="onToggleTodoStatus">
         </todo-list>
+        <todo-footer v-on:removeAll="onClearAll"></todo-footer>
     </div>
 </template>
 
@@ -14,13 +16,15 @@
 import TodoHeader from '../components/todo/TodoHeader.vue'
 import TodoInput from '../components/todo/TodoInput.vue'
 import TodoList from '../components/todo/TodoList.vue'
+import TodoFooter from '../components/todo/TodoFooter.vue'
 import { mapActions } from 'vuex'
 export default {
     name: 'Todo',
     components: {
         'todo-header': TodoHeader,
         'todo-input': TodoInput,
-        'todo-list': TodoList
+        'todo-list': TodoList,
+        'todo-footer': TodoFooter
     },
     methods: {
         // store에서 중앙 관리를 하고
@@ -29,7 +33,9 @@ export default {
             'addTodo',
             'removeTodo',
             'editTodo',
-            'save'
+            'save',
+            'clearAll',
+            'toggleTodoStatus'
         ]),
         onAddTodo (content) {
             const todoItem = { content }
@@ -42,6 +48,14 @@ export default {
         },
         onEditTodo (content, id) {
             this.editTodo({ id, content })
+            this.save()
+        },
+        onClearAll () {
+            this.clearAll()
+            this.save()
+        },
+        onToggleTodoStatus(id) {
+            this.toggleTodoStatus(id)
             this.save()
         }
     }
---
+++ javascript/JonghyeonYoo/jsmain/src/main/java/com/example/jsmain/repository/RandNumRepository.java
@@ -0,0 +1,13 @@
+package com.example.jsmain.repository;
+
+import com.example.jsmain.entity.RandNumMessage;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RandNumRepository {
+
+    public RandNumMessage getRandom() {
+        return new RandNumMessage();
+    }
+
+}

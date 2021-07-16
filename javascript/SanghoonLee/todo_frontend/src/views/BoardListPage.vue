6c9dce02934e0eab567f361d6d3087fae056e7fc
silenc3502 <gcccompil3r@gmail.com>
2021-07-16 19:05:10 +0900

    add BoardList

--- javascript/SanghoonLee/todo_frontend/src/views/BoardListPage.vue
+++ javascript/SanghoonLee/todo_frontend/src/views/BoardListPage.vue
@@ -4,19 +4,28 @@
         <router-link :to="{ name: 'BoardRegisterPage' }">
             게시물 작성
         </router-link>
-        <!-- <board-list/> -->
+        <board-list :boards="boards"/>
     </div>
 </template>
 
 <script>
 
-// import { mapState, mapActions } from 'vuex'
+import BoardList from '@/components/board/BoardList.vue'
+import { mapState, mapActions } from 'vuex'
 
 export default {
     name: 'BoardListPage',
     components: {
+        BoardList
     },
     computed: {
+        ...mapState(['boards'])
+    },
+    mounted () {
+        this.fetchBoardList()
+    },
+    methods: {
+        ...mapActions(['fetchBoardList'])
     }
 }
 

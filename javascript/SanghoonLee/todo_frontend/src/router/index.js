0f004885cd58c5694298d98e99850ccf2aebc69f
silenc3502 <gcccompil3r@gmail.com>
2021-07-19 16:42:31 +0900

    frontend

--- javascript/SanghoonLee/todo_frontend/src/router/index.js
+++ javascript/SanghoonLee/todo_frontend/src/router/index.js
@@ -9,6 +9,7 @@ import Homework from '../views/Homework.vue'
 import BoardListPage from '@/views/BoardListPage.vue'
 import BoardRegisterPage from '@/views/BoardRegisterPage.vue'
 import BoardReadPage from '@/views/BoardReadPage.vue'
+import BoardModifyPage from '@/views/BoardModifyPage.vue'
 
 // game
 import GameMemberRegisterPage from '@/views/GameMemberRegisterPage.vue'
@@ -57,6 +58,17 @@ const routes = [
       default: BoardReadPage
     },
     props: {
+      default: true
+    }
+  },
+  {
+    path: '/board/:boardNo/edit',
+    name: 'BoardModifyPage',
+    components: {
+      default: BoardModifyPage
+    },
+    props: {
+      // 라우팅 파라미터를 props에 자동으로 설정하게 서포트함
       default: true
     }
   },

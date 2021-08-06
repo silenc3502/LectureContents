c948eaca2647b6c5d4c90be0140fc0ac580dc390
silenc3502 <gcccompil3r@gmail.com>
2021-08-06 19:25:45 +0900

    65: Vue + Join Fetch Page

--- javascript/SanghoonLee/todo_frontend/src/views/MemberJoinColumnTestPage.vue
+++ javascript/SanghoonLee/todo_frontend/src/views/MemberJoinColumnTestPage.vue
@@ -4,6 +4,13 @@
             <h2>회원 가입</h2>
         </div>
         <member-join-column-test-form @submit="onSubmit"/>
+        <v-spacer></v-spacer>
+        <v-btn tile color="teal" @click="onJPATest">
+            <v-icon left>
+                ads_click
+            </v-icon>
+            클릭하세요!
+        </v-btn>
     </div>
 </template>
 
@@ -31,6 +38,17 @@ export default {
                             params: { boardNo: res.data.boardNo.toString() }
                         })
                         */
+                    })
+                    .catch(res => {
+                        alert(res.response.data.message)
+                    })
+        },
+        onJPATest () {
+            axios.post('http://localhost:7777/jpamember/test', {
+                        userId: null, password: null, auth: null
+                    })
+                    .then(res => {
+                        alert('테스트 성공! - ' + res)
                     })
                     .catch(res => {
                         alert(res.response.data.message)

38327b6d385eea9127f1c35b4d48627b017d15b1
silenc3502 <gcccompil3r@gmail.com>
2021-11-12 18:08:54 +0900

    sb + jpa + vue + ts 누락 부분 추가

---
+++ vuejs/SanghoonLee/tstest/src/components/board/BoardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <table border="1">
+      <tr>
+        <th align="center" width="80">No</th>
+        <th align="center" width="320">제목</th>
+        <th align="center" width="100">작성자</th>
+        <th align="center" width="180">등록일자</th>
+      </tr>
+
+      <tr v-if="!boards || (Array.isArray(boards) && boards.length === 0)">
+        <td colspan="4">
+          어떤 데이터도 없습니다!
+        </td>
+      </tr>
+
+      <tr v-else v-for="board in boards" :key="board.boardNo">
+        <td align="center">{{ board.boardNo }}</td>
+        <td align="center">{{ board.title }}</td>
+        <!-- <td align="center">{{ board.title }}</td> -->
+        <td align="center">{{ board.writer }}</td>
+        <td align="center">{{ board.regDate }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator'
+  import { Board } from '@/store'
+
+  @Component
+  export default class BoardList extends Vue {
+    @Prop() private boards!: Board[]
+  }
+</script>
---
+++ vuejs/SanghoonLee/tstest/src/views/board/TypeScriptBoardListPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div align="center">
+    <h2>Board(게시판) 리스팅</h2>
+    <!-- <router-link :to="{ name: BoardRegisterPage }">글쓰기</router-link> -->
+    <board-list :boards="boards"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import { State, Action } from 'vuex-class'
+  // npm install vuex-class --save-dev
+  import BoardList from '@/components/board/BoardList.vue'
+  import { Board } from '@/store'
+
+  @Component({
+    components: {
+      BoardList
+    }
+  })
+
+  export default class TypeScriptBoardListPage extends Vue {
+    @State readonly boards!: Board[]
+
+    @Action readonly fetchBoardList: any
+
+    created () {
+      this.fetchBoardList()
+    }
+  }
+</script>
